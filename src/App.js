4b2a27d20e1152cd44d662d181c65736a7f941ff
Mike <michael.dobinson@gmail.com>
2023-04-18 14:11:56 +0100

    set up browser routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Reviews from './components/Reviews';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Reviews />
+      <Routes>
+        <Route path="/" element={<Reviews />} />
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
     </div>
   );
 }
