ca574fc1524785b1910ea92968a391f3a376d7b4
Mike <michael.dobinson@gmail.com>
2023-04-20 13:35:42 +0100

    add increasing vote button on articles

--- src/components/ArtlcleCard.jsx
+++ src/components/ArtlcleCard.jsx
@@ -4,10 +4,11 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import CommentList from './CommentList';
+import Button from '@mui/material/Button';
 
 export default function ArticleCard() {
   const { article_id } = useParams();
-
+  const [votesToAdd, setVotesToAdd] = useState(0);
   const [article, setArticle] = useState({});
 
   useEffect(() => {
@@ -16,16 +17,25 @@ export default function ArticleCard() {
     });
   }, [article_id]);
 
+  const handleClick = (event) => {
+    event.target.disabled = true;
+    setVotesToAdd(1);
+    api.patchArticleVotes(article_id, 1);
+  };
   return (
     <div>
+      <br />
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        Like This Article
+      </Button>
       <h2>Article</h2>
       <ul className="article-list">
         <li key={article.article_id}>
           <h3>{article.title}</h3>
-          <p>Written by: {article.owner}</p>
+          <p>Written by: {article.author}</p>
           <p>{article.article_body}</p>
           <p>Comments: {article.comment_count}</p>
-          <p>Likes: {article.votes}</p>
+          <p>Likes: {article.votes + votesToAdd}</p>
           <p>Topic: {article.topic}</p>
           <CommentList article_id={article_id} />
         </li>
